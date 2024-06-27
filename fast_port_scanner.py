6cc7cc7722cf16f5bbbd77a495b7e12269ff35f4
Jayesh Gaba <jg9140@srmist.edu.in>
2024-06-27 13:28:16 +0530

    added service running on port

--- fast_port_scanner.py
+++ fast_port_scanner.py
@@ -4,6 +4,7 @@ import time
 import queue
 import threading
 from termcolor import cprint
+import keyboard
 
 usage = "Usage - python3 port_scanner.py TARGET START_PORT END_PORT Threads"
 
@@ -16,10 +17,10 @@ ban = """\n
 #+#        #+#    #+# #+#    #+#    #+#          #+#    #+# #+#    #+# #+#     #+# #+#   #+#+# #+#   #+#+# #+#        #+#    #+# 
 ###         ########  ###    ###    ###           ########   ########  ###     ### ###    #### ###    #### ########## ###    ### \n
 """
-cprint(ban, 'light_green')
+cprint(ban, "light_green")
 
 if len(sys.argv) != 5:
-    cprint(usage, 'red', attrs=['bold'])
+    cprint(usage, "red", attrs=["bold"])
     sys.exit()
 
 target = sys.argv[1]
@@ -27,42 +28,42 @@ target = sys.argv[1]
 try:
     start_port = int(sys.argv[2])
 except ValueError:
-    cprint("[*] Invalid START_PORT. Please provide an integer value.", 'red')
+    cprint("[*] Invalid START_PORT. Please provide an integer value.", "red")
     sys.exit()
-      
+
 if start_port < 0 or start_port > 65535:
-    cprint("[*] START_PORT must be in the range 0-65535.", 'red')
+    cprint("[*] START_PORT must be in the range 0-65535.", "red")
     sys.exit()
 
 
 try:
     end_port = int(sys.argv[3])
 except ValueError:
-    cprint("[*] Invalid END_PORT. Please provide an integer value.", 'red')
+    cprint("[*] Invalid END_PORT. Please provide an integer value.", "red")
     sys.exit()
-    
+
 if end_port < 0 or end_port > 65535:
-    cprint("[*] END_PORT must be in the range 0-65535.", 'red')
+    cprint("[*] END_PORT must be in the range 0-65535.", "red")
     sys.exit()
 
 if start_port > end_port:
-    cprint("[*] START_PORT must be less than or equal to END_PORT.", 'red')
+    cprint("[*] START_PORT must be less than or equal to END_PORT.", "red")
     sys.exit()
 
 
 try:
     thread_no = int(sys.argv[4])
 except ValueError:
-    cprint("[*] Invalid number of Threads. Please provide an integer value.", 'red')
+    cprint("[*] Invalid number of Threads. Please provide an integer value.", "red")
     sys.exit()
 
 if thread_no <= 0:
-    cprint("[*] Number of Threads must be greater than 0.", 'red')
+    cprint("[*] Number of Threads must be greater than 0.", "red")
     sys.exit()
 
 
-banner = "\n[+] Result: "
-result = "\nPORT\tSTATE\n"
+res = "\n[+] Result: "
+result = "\nPORT\tSTATE\tSERVICE\n"
 
 try:
     target = socket.gethostbyname(target)
@@ -71,6 +72,16 @@ except socket.gaierror:
     sys.exit()
 
 print(f"Scanning target: {target}")
+
+
+def get_banner(port, s):
+    try:
+        s.settimeout(2)
+        banner = s.recv(1024).decode().strip()
+        return banner
+    except:
+        return "Unknown"
+
 
 def scan_port():
     global result
@@ -84,11 +95,14 @@ def scan_port():
             s.settimeout(1)
             conn = s.connect_ex((target, port))
             if not conn:
-                result += f"{port}\tOPEN\n"
+                banner = get_banner(port, s)
+                banner = ''.join(banner.splitlines())
+                result += f"{port}\tOPEN\t{banner}\n"
             s.close()
         except:
             pass
         q.task_done()
+
 
 q = queue.Queue()
 threads = []
@@ -111,27 +125,21 @@ for t in threads:
     t.join()
 
 end_time = time.time()
-cprint(banner, 'red', attrs=['blink'])
-cprint(result, 'cyan')
+cprint(res, "red", attrs=["blink"])
+cprint(result, "cyan")
 print(f"Time taken for scan: {end_time - start_time:.2f} seconds")
 
 attempts = 0
 while attempts < 3:
-    cprint("[+] Do you want the results to be output in a file? [Y/N]", 'cyan')
-    user_input = input().upper()
-
-    if user_input == "Y":
-        with open("ports.txt", "w") as file:
-            file.write(f"Port scan results for target: {target}\n")
-            file.write(result)
-        cprint("[+] Written to file ports.txt.", 'red')
-        break
-
-    elif user_input == "N":
-        exit()
-
-    else:
-        cprint("*****Please input Y/N for desired output*****", 'red')
-        attempts += 1
-
-cprint("[+] Exiting the program.", 'red')
+    cprint("[+] Do you want the results to be output in a file? [Y/N]", "cyan")
+    while True:
+        if keyboard.is_pressed('Y') or keyboard.is_pressed('y'):
+            with open("ports.txt", "w") as file:
+                file.write(f"Port scan results for target: {target}\n")
+                file.write(result)
+            cprint("[+] Written to file ports.txt.", "red")
+            break
+        elif keyboard.is_pressed('N') or keyboard.is_pressed('n'):
+            cprint("[+] Exiting the program.", "red")
+            sys.exit()
+    break
